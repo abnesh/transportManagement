b12952f5429b73d9deb6502efc93b1e1d9c6f353
Pankaj Bhushan <1pankaj4@gmail.com>
2018-07-26 15:34:07 +0530

    Enabled Eureka discovery-service

--- eureka-service/src/main/java/com/microservices/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/microservices/eurekaservice/EurekaServiceApplication.java
@@ -2,7 +2,9 @@ package com.microservices.eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServiceApplication {
 

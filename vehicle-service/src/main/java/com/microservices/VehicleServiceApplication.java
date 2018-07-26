574594cf9b0043dcf2bd3139fce3672c7378c73b
Pankaj Bhushan <1pankaj4@gmail.com>
2018-07-26 15:15:34 +0530

    Initial commit for vehicle-service

---
+++ vehicle-service/src/main/java/com/microservices/VehicleServiceApplication.java
@@ -0,0 +1,12 @@
+package com.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleServiceApplication.class, args);
+	}
+}

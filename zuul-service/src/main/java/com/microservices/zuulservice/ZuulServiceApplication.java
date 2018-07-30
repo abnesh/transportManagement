a537076ba8a38b4c6fed2d07cfa977079d242db9
Pankaj Bhushan <1pankaj4@gmail.com>
2018-07-30 15:06:03 +0530

    Initial Zuul Commit

---
+++ zuul-service/src/main/java/com/microservices/zuulservice/ZuulServiceApplication.java
@@ -0,0 +1,12 @@
+package com.microservices.zuulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServiceApplication.class, args);
+	}
+}
